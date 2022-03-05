1ae0dfad4c075e99cefb140a94ae942d25651372
jwenT <56579875+jwenT96@users.noreply.github.com>
2022-03-05 23:13:21 +0800

    UI Update to Present Flower + Share Files
    Now currently the scan image just opens the camera. It does not have the extra page

--- lib/present_flower.dart
+++ lib/present_flower.dart
@@ -1,11 +1,15 @@
 import 'dart:io';
 
+import 'package:blossom/scan_flower.dart';
 import 'package:blossom/social_media.dart';
 import 'package:flutter/material.dart';
 import 'backend/flower.dart';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:blossom/components/app_text.dart';
+import 'package:blossom/constants.dart';
+import 'package:image_watermark/image_watermark.dart';
 
 class PresentFlower extends StatefulWidget {
   const PresentFlower({Key? key}) : super(key: key);
@@ -22,6 +26,9 @@ class _PresentFlowerState extends State<PresentFlower> {
       funFact2 = "",
       funFact3 = "",
       numScans = "";
+  var scannedImage = 'https://picsum.photos/id/237/480/640';
+  var databaseImage = 'https://picsum.photos/id/237/480/640';
+  bool favourite = false;
 
   @override
   void initState() {
@@ -35,6 +42,7 @@ class _PresentFlowerState extends State<PresentFlower> {
         funFact2 = flower["fun_facts_2"];
         funFact3 = flower["fun_facts_3"];
         numScans = flower["num_scans"].toString();
+        favourite = false;
       });
     });
   }
@@ -52,23 +60,15 @@ class _PresentFlowerState extends State<PresentFlower> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("The flower is..."),
-          Text(
-            flowerName,
-            style: TextStyle(fontSize: 30),
-            textAlign: TextAlign.left,
-          ),
-          Text("I have been scanned " + numScans + " times!\n"),
-          Text(
-            "Scientific Name",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          Text(scientificName + "\n"),
-          Text(
-            "Native to ",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          Text(nativeTo),
+          AppTextBold(size: 17, text: "I am a"),
+          AppTextBold(size: 30, text: flowerName, color: kAppBrownColor),
+          AppTextNormal(
+              size: 14, text: "I have been scanned " + numScans + " times!\n"),
+          AppTextNormal(
+              size: 12, text: "Scientific Name", color: kAppBrownColor),
+          AppTextBold(size: 14, text: scientificName + '\n'),
+          AppTextNormal(size: 12, text: "Native to", color: kAppBrownColor),
+          AppTextBold(size: 14, text: nativeTo + '\n'),
         ],
       ),
     );
@@ -78,49 +78,47 @@ class _PresentFlowerState extends State<PresentFlower> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            "Flower facts:",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            textAlign: TextAlign.left,
-          ),
+          AppTextBold(size: 20, text: "Flower facts"),
           SizedBox(height: 10),
-          Text("1: " + funFact1),
+          AppTextNormal(size: 12, text: "1. " + funFact1),
           SizedBox(height: 10),
-          Text("2: " + funFact2),
+          AppTextNormal(size: 12, text: "2. " + funFact2),
           SizedBox(height: 10),
-          Text("3: " + funFact3),
+          AppTextNormal(size: 12, text: "3. " + funFact1)
         ],
       ),
     );
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
-        centerTitle: true,
+        iconTheme: IconThemeData(color: Colors.black),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => ScanFlower()));
+          },
+        ),
+        backgroundColor: Colors.white,
         actions: [
           IconButton(
-            icon: Icon(Icons.share),
+            icon: favourite
+                ? Icon(Icons.favorite, color: kButtonColor1)
+                : Icon(Icons.favorite_border),
             onPressed: () async {
-              showDialog(
-                  context: context,
-                  builder: (context) {
-                    return new ShareSocialMedia();
-                  });
+              setState(() => favourite = !favourite);
+              // showDialog(
+              //     context: context,
+              //     builder: (context) {
+              //       return new ShareSocialMedia();
+              //     });
             },
           ),
           IconButton(
-            icon: Icon(Icons.favorite_border),
+            icon: Icon(Icons.share),
             onPressed: () async {
-              final urlImage = "https://picsum.photos/200/300";
-              final url = Uri.parse(urlImage);
-              final response = await http.get(url);
-              final bytes = response.bodyBytes;
-
-              final temp = await getTemporaryDirectory();
-              final path = '${temp.path}/image.jpg';
-
-              File(path).writeAsBytesSync(bytes);
-              await Share.shareFiles([path], text: "This lion is cute!");
+              share(flowerName, scannedImage);
+              print('Ran share');
             },
           )
         ],
@@ -132,29 +130,29 @@ class _PresentFlowerState extends State<PresentFlower> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Expanded(flex: 6, child: flowerInfo),
                     Expanded(
                         flex: 4,
                         child: Container(
-                            color: Colors.blue,
-                            child: Image.network(
-                                'https://picsum.photos/250?image=9'))),
+                            padding: EdgeInsets.only(top: 25),
+                            child: Image.network(scannedImage))),
+                    Expanded(flex: 6, child: flowerInfo),
                   ],
                 ),
                 Divider(height: 20),
                 Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Expanded(
-                        flex: 4,
-                        child: Container(
-                            color: Colors.red,
-                            child: Image.network(
-                                'https://picsum.photos/250?image=9'))),
                     Expanded(
                       flex: 6,
                       child: flowerFacts,
                     ),
+                    Expanded(
+                        flex: 4,
+                        child: Container(
+                            padding: EdgeInsets.only(top: 25),
+                            child: Image.network(databaseImage))),
                   ],
                 ),
               ]),
@@ -162,6 +160,29 @@ class _PresentFlowerState extends State<PresentFlower> {
       ),
     );
   }
+}
+
+void share(String flowerName, scannedImage) async {
+  final urlImage = scannedImage;
+  final url = Uri.parse(urlImage);
+  final response = await http.get(url);
+  final bytes = response.bodyBytes;
+  final watermarked =
+      await image_watermark.addTextWatermarkCentered(bytes, 'Taken by Blossom');
+  final appurl = "google.com";
+
+  final temp = await getTemporaryDirectory();
+  final path = '${temp.path}/image.jpg';
+
+  File(path).writeAsBytesSync(watermarked);
+  // await Share.share(
+  //     "I identified " + flowerName + " using the Blossom app!" + appurl);
+  await Share.shareFiles([path],
+      text: "I identified " +
+          flowerName +
+          " using the Blossom app!" +
+          "Find out more about Blossom at: " +
+          appurl);
 }
 
 //Image.network('https://picsum.photos/250?image=9'),
--- lib/scan_flower.dart
+++ lib/scan_flower.dart
@@ -1,6 +1,10 @@
+import 'package:blossom/dashboard.dart';
+import 'package:blossom/present_flower.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
+
+import 'home.dart';
 
 class ScanFlower extends StatefulWidget {
   const ScanFlower({Key? key}) : super(key: key);
@@ -13,74 +17,79 @@ class _ScanFlowerState extends State<ScanFlower> {
   @override
   File? imageFile;
   @override
+  void initState() {
+    super.initState();
+    getImage(source: ImageSource.camera);
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Capturing Images'),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (imageFile != null)
-              Container(
-                width: 640,
-                height: 480,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  image: DecorationImage(
-                      image: FileImage(imageFile!), fit: BoxFit.cover),
-                  border: Border.all(width: 8, color: Colors.black),
-                  borderRadius: BorderRadius.circular(12.0),
-                ),
-              )
-            else
-              Container(
-                width: 640,
-                height: 480,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  border: Border.all(width: 8, color: Colors.black12),
-                  borderRadius: BorderRadius.circular(12.0),
-                ),
-                child: const Text(
-                  'Image should appear here',
-                  style: TextStyle(fontSize: 26),
-                ),
-              ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                      onPressed: () => getImage(source: ImageSource.camera),
-                      child: const Text('Capture Image',
-                          style: TextStyle(fontSize: 18))),
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                Expanded(
-                  child: ElevatedButton(
-                      onPressed: () => () {},
-                      child:
-                          const Text('Next', style: TextStyle(fontSize: 18))),
-                )
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
+        // body: Padding(
+        //   padding: const EdgeInsets.all(12.0),
+        //   child: Column(
+        //     mainAxisAlignment: MainAxisAlignment.center,
+        //     children: [
+        //       if (imageFile != null)
+        //         Container(
+        //           width: 640,
+        //           height: 480,
+        //           alignment: Alignment.center,
+        //           decoration: BoxDecoration(
+        //             color: Colors.grey,
+        //             image: DecorationImage(
+        //                 image: FileImage(imageFile!), fit: BoxFit.cover),
+        //             border: Border.all(width: 8, color: Colors.black),
+        //             borderRadius: BorderRadius.circular(12.0),
+        //           ),
+        //         )
+        //       else
+        //         Container(
+        //           width: 640,
+        //           height: 480,
+        //           alignment: Alignment.center,
+        //           decoration: BoxDecoration(
+        //             color: Colors.grey,
+        //             border: Border.all(width: 8, color: Colors.black12),
+        //             borderRadius: BorderRadius.circular(12.0),
+        //           ),
+        //           child: const Text(
+        //             'Image is nothing',
+        //             style: TextStyle(fontSize: 26),
+        //           ),
+        //         ),
+        //       const SizedBox(
+        //         height: 20,
+        //       ),
+        //       Row(
+        //         children: [
+        //           Expanded(
+        //             child: ElevatedButton(
+        //                 onPressed: () => getImage(source: ImageSource.camera),
+        //                 child: const Text('Capture Image',
+        //                     style: TextStyle(fontSize: 18))),
+        //           ),
+        //           const SizedBox(
+        //             width: 20,
+        //           ),
+        //           Expanded(
+        //             child: ElevatedButton(
+        //                 onPressed: () {
+        //                   Navigator.of(context).push(MaterialPageRoute(
+        //                       builder: (context) => PresentFlower()));
+        //                 },
+        //                 child:
+        //                     const Text('Next', style: TextStyle(fontSize: 18))),
+        //           )
+        //         ],
+        //       ),
+        //     ],
+        //   ),
+        // ),
+        );
   }
 
   void getImage({required ImageSource source}) async {
+    final navigator = Navigator.of(context);
     final file = await ImagePicker().pickImage(
         source: source,
         maxWidth: 640,
@@ -92,6 +101,14 @@ class _ScanFlowerState extends State<ScanFlower> {
       setState(() {
         imageFile = File(file!.path);
       });
+    }
+
+    if (imageFile != null) {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => PresentFlower()));
+    } else {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => Dashboard()));
     }
   }
 }
