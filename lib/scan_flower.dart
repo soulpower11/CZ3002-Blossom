77251448005db905a778cba72d6f1ea9db7e15b0
soulpower11 <5299979+soulpower11@users.noreply.github.com>
2022-03-06 00:21:47 +0800

    Fixed wrong merge

--- lib/scan_flower.dart
+++ lib/scan_flower.dart
@@ -1,5 +1,8 @@
 import 'package:blossom/dashboard.dart';
 import 'package:blossom/present_flower.dart';
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 
 import 'home.dart';
 
@@ -93,21 +96,6 @@ class _ScanFlowerState extends State<ScanFlower> {
         maxHeight: 480,
         imageQuality: 100 //0 - 100
         );
-
-    var bytes = await file?.readAsBytes();
-    var tags = await readExifFromBytes(bytes!);
-
-    if (tags.isEmpty) {
-      print("No EXIF information found");
-    }
-
-    if (tags.containsKey('TAG_GPS_LATITUDE')) {
-      print('File has JPEG thumbnail');
-    }
-    if (tags.containsKey('TIFFThumbnail')) {
-      print('File has TIFF thumbnail');
-      tags.remove('TIFFThumbnail');
-    }
 
     if (file?.path != null) {
       setState(() {
