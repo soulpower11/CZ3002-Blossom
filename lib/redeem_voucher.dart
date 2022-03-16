f154d381b69e658fcc2f08b7faf40e505d3fff3c
Liu Zhixuan <liuzhixuan21@gmail.com>
2022-03-16 23:11:03 +0800

    update AlertDialog and text/button format

--- lib/redeem_voucher.dart
+++ lib/redeem_voucher.dart
@@ -1,4 +1,7 @@
+import 'package:blossom/components/app_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:blossom/constants.dart';
 
 class RedeemVoucher extends StatefulWidget {
   const RedeemVoucher({Key? key}) : super(key: key);
@@ -10,6 +13,80 @@ class RedeemVoucher extends StatefulWidget {
 class _RedeemVoucherState extends State<RedeemVoucher> {
   @override
   Widget build(BuildContext context) {
+    void _showSuccesssScreen() {
+      showDialog(
+        context: context,
+        //barrierDismissible: false,//user must tap button to dismiss
+        builder: (_) => AlertDialog(
+          title: const Text(
+            "Congratulations",
+            textAlign: TextAlign.center,
+          ),
+          content: SingleChildScrollView(
+            child: Column(
+              children: const <Widget>[
+                Text(
+                  "Here's the code for your voucher ........",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                SelectableText(
+                  '\nSampleVoucherCode',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            Center(
+              child: ElevatedButton(
+                child: const Text("Use"),
+                onPressed: () {
+                  Navigator.pop(context, 'use');
+                  //copy code to your clipboard
+                  Clipboard.setData(
+                          new ClipboardData(text: "SampleVoucherCode"))
+                      .then((_) {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content:
+                            Text('Voucher code copied to your clipboard !')));
+                  });
+                },
+              ),
+            )
+          ],
+          elevation: 24.0,
+          //backgroundColor:
+        ),
+      );
+    }
+
+    void _showFailureScreen() {
+      showDialog(
+        context: context,
+        //barrierDismissible: false,//user must tap button to dismiss
+        builder: (_) => AlertDialog(
+          title: const Text("Sorry"),
+          content: const Text(
+              "You don't have enough points to redeem this voucher."),
+          actions: [
+            TextButton(
+              child: const Text("Ok"),
+              onPressed: () {
+                Navigator.pop(context, 'Ok');
+              },
+            ),
+          ],
+          elevation: 24.0,
+          //backgroundColor:
+        ),
+      );
+    }
+
     Row VoucherTile(int index) {
       int points = 2; //get the number of points needed to redeem this voucher
       return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
@@ -26,19 +103,56 @@ class _RedeemVoucherState extends State<RedeemVoucher> {
         ),
         Center(
           //voucher info
-          child: Text("Voucher " +
-              (index + 1).toString() +
-              " info\n" +
-              points.toString() +
-              " points"),
+          child: AppTextNormal(
+            text: "Voucher " +
+                (index + 1).toString() +
+                " info\n" +
+                points.toString() +
+                " points",
+            size: 15,
+          ),
         ),
         Center(
           //redeem button
           child: ElevatedButton(
             onPressed: () {
-              // Respond to button press
+              //total points >= points(voucher): can redeem
+              bool canRedeem = true;
+              if (canRedeem) {
+                showDialog(
+                  context: context,
+                  //barrierDismissible: false,//user must tap button to dismiss
+                  builder: (_) => AlertDialog(
+                    title: const Text("Confirm"),
+                    content: Text(points.toString() +
+                        " points will be deducted from your account if you confirm to redeem Voucher xxx"),
+                    actions: [
+                      TextButton(
+                        child: const Text("Cancel"),
+                        onPressed: () => Navigator.pop(context, 'Cancel'),
+                      ),
+                      TextButton(
+                        child: const Text("Confirm to Redeem"),
+                        onPressed: () {
+                          //redeem voucher code: deduct points; -> my voucher list;
+                          Navigator.pop(context, 'Redeem');
+                          _showSuccesssScreen();
+                        },
+                      ),
+                    ],
+                    elevation: 24.0,
+                    //backgroundColor:
+                  ),
+                );
+              } else {
+                _showFailureScreen();
+              }
             },
             child: const Text('Redeem'),
+            style: ElevatedButton.styleFrom(
+              primary: kButtonColor1, // Background color
+              onPrimary: Colors.white, // Text Color (Foreground color)
+            ),
           ),
         ),
       ]);
@@ -56,7 +170,7 @@ class _RedeemVoucherState extends State<RedeemVoucher> {
         child: ListView.builder(
             itemCount: 15,
             itemBuilder: (BuildContext context, int index) {
-              return Container(
+              return SizedBox(
                 height: 90,
                 child: VoucherTile(index),
               );
